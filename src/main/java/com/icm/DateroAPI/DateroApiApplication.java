de3fa62a46f28fbb547adb9806c7fb00bdc99957
Eduardo Aguilar <es123y123@gmail.com>
2024-02-02 16:21:17 -0500

    MQTT Configurado

--- src/main/java/com/icm/DateroAPI/DateroApiApplication.java
+++ src/main/java/com/icm/DateroAPI/DateroApiApplication.java
@@ -1,13 +1,22 @@
 package com.icm.DateroAPI;
 
+import com.icm.DateroAPI.MQTT.MqttSubscriber;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DateroApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DateroApiApplication.class, args);
+
+		// SpringApplication.run(DateroApiApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(DateroApiApplication.class, args);
+
+		MqttSubscriber mqttSubscriber = context.getBean(MqttSubscriber.class);
+		// Llama a subscribeToTopic en la instancia
+		mqttSubscriber.subscribeToTopic("prueba");
 	}
 
 }
